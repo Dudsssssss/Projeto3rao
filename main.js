d6e66bc9108295d108b9a2c4c3db290c38c88d16
Dudsssssss <105220552+Dudsssssss@users.noreply.github.com>
2024-04-02 08:12:36 -0300

    f

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const botoes = document.querySelectorAll(".aba-conteudo");
 for( let i=0; i <botoes.length; i++){
 botoes[i].onclick = function(){
  for( let j=0; j< botoes.length; j++) {
-   botoes[j].classlist.remove("ativo");
-   textos[j].classlist.remove("ativo");
+   botoes[j].classList.remove("ativo");
+   textos[j].classList.remove("ativo");
  }
-  botoes[i].classlist.add("ativo") 
-  textos[i].classlist.add("ativo");
+  botoes[i].classList.add("ativo") 
+  textos[i].classList.add("ativo");
 }
 }
