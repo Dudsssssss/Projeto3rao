b1d7c2c857315397907dda2f8f64d5948c0f90b3
Dudsssssss <105220552+Dudsssssss@users.noreply.github.com>
2024-04-30 08:05:26 -0300

    sdsf

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ for( let i= 0; i < botoes.length; i++){
 }
 const contadores = document.querrySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-22T00:00:00");
-let tempoAtual = new Date();
+
+const tempos = [
+    tempoObjetivo1
+]
 
 
 for (let i= 0; i< contadores.length; i++){
